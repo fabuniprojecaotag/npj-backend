a6d4f42f2e082e0a572e8a09e386a6890788e238
Jonatas <jonatasmateuscontato@gmail.com>
2024-05-03 11:10:47 -0300

    Rollback do UsuarioService

--- src/main/java/com/uniprojecao/fabrica/gprojuridico/services/UsuarioService.java
+++ src/main/java/com/uniprojecao/fabrica/gprojuridico/services/UsuarioService.java
@@ -2,11 +2,9 @@ package com.uniprojecao.fabrica.gprojuridico.services;
 
 import com.uniprojecao.fabrica.gprojuridico.domains.usuario.Usuario;
 import com.uniprojecao.fabrica.gprojuridico.dto.min.UsuarioMinDTO;
-import com.uniprojecao.fabrica.gprojuridico.dto.usuario.EstagiarioDTO;
 import com.uniprojecao.fabrica.gprojuridico.dto.usuario.UsuarioDTO;
 import com.uniprojecao.fabrica.gprojuridico.repository.UsuarioRepository;
 import com.uniprojecao.fabrica.gprojuridico.services.exceptions.UserAlreadyCreatedException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,40 +12,42 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 import static com.uniprojecao.fabrica.gprojuridico.services.utils.UsuarioUtils.*;
 import static com.uniprojecao.fabrica.gprojuridico.services.utils.Utils.encryptPassword;
 import static com.uniprojecao.fabrica.gprojuridico.services.utils.Utils.initFilter;
+import static com.uniprojecao.fabrica.gprojuridico.services.utils.Constants.USUARIOS_COLLECTION;
 import static java.lang.Integer.parseInt;
 
 @Service
-public class UsuarioService implements UserDetailsService {
+public class UsuarioService extends BaseService implements UserDetailsService {
 
-    @Autowired
-    private UsuarioRepository repository;
+    private final UsuarioRepository REPOSITORY;
+    private final String COLLECTION_NAME;
 
-    private final String COLLECTION_NAME = "usuarios";
+    public UsuarioService(UsuarioRepository repository) {
+        super(repository, USUARIOS_COLLECTION);
+        REPOSITORY = repository;
+        COLLECTION_NAME = USUARIOS_COLLECTION;
+    }
 
     public UsuarioDTO insert(UsuarioDTO dto) {
-        var id = (!(dto instanceof EstagiarioDTO e)) ? dto.getEmail().replaceAll("@projecao.br", "") : e.getMatricula();
-        dto.setId(id);
+        var id = dto.getEmail();
 
         verifyIfExistsUserInDatabase(dto, id);
         encryptPassword(dto);
 
-        repository.save(COLLECTION_NAME, id, dtoToUsuario(dto));
+        REPOSITORY.save(COLLECTION_NAME, id, dtoToUsuario(dto));
         return dto;
     }
 
     public List<UsuarioMinDTO> findAll(String limit, String field, String filter, String value) {
-        return repository.findAll(parseInt(limit), initFilter(field, filter, value));
+        return REPOSITORY.findAll(parseInt(limit), initFilter(field, filter, value));
     }
 
     public UsuarioDTO findById(String id) {
-        var usuario = repository.findById(id);
-        return (usuario != null) ? usuarioToDto(usuario) : null;
+        return usuarioToDto(REPOSITORY.findById(id));
     }
 
     private void verifyIfExistsUserInDatabase(UsuarioDTO dto, String id) {
@@ -68,25 +68,13 @@ public class UsuarioService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        return repository.findById(username);
+        return REPOSITORY.findById(username);
     }
 
     public UsuarioDTO authenticated() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Usuario usuario = (Usuario) authentication.getPrincipal();
-        String id = usuario.getId();
+        String id = usuario.getEmail();
         return findById(id);
-    }
-
-    public void update(String id, Map<String, Object> data) {
-        repository.update(COLLECTION_NAME, id, data);
-    }
-
-    public void delete(String id) {
-        repository.delete(COLLECTION_NAME, id);
-    }
-
-    public void deleteAll(String limit, String field, String filter, String value) {
-        repository.deleteAll(COLLECTION_NAME, null, parseInt(limit), initFilter(field, filter, value));
     }
 }
