65ce22917d8e1fe2e1cc907d3f9c7f8867968680
Jonatas <jonatasmateuscontato@gmail.com>
2024-05-03 22:42:47 -0300

    Javadoc adicionado para a classe UsuarioService

--- src/main/java/com/uniprojecao/fabrica/gprojuridico/services/UsuarioService.java
+++ src/main/java/com/uniprojecao/fabrica/gprojuridico/services/UsuarioService.java
@@ -20,6 +20,39 @@ import static com.uniprojecao.fabrica.gprojuridico.services.utils.Utils.initFilt
 import static com.uniprojecao.fabrica.gprojuridico.services.utils.Constants.USUARIOS_COLLECTION;
 import static java.lang.Integer.parseInt;
 
+/**
+ * Baseado no padrão arquitetural camada de serviço, este serviço tem por
+ * objetivo apresentar a implementação de regras e lógica de negócio. <p>
+ *
+ * Este serviço em si trata da interação com o modelo de domínio {@link Usuario},
+ * onde as regras de negócios pertinentes a este domínio estão listadas logo
+ * abaixo: <p>
+ *
+ * <h2>Regras de Negócio</h2>
+ *
+ * <h3>Verificar Usuário</h3>
+ * Ao inserir um usuário, deverá ser verificado se este existe no banco de dados.
+ * Caso exista, uma exceção deverá ser lançada; senão o fluxo segue-se normalmente. <p>
+ *
+ * <h3>Gerar Senha Temporária</h3>
+ * Caso o DTO recebido não apresente a senha preenchida, uma senha aleatória deverá
+ * ser gerada. Além disso, o usuário deve receber, por e-mail, suas credenciais de
+ * acesso, contendo: nome de usuário, e-mail e senha temporária. <p>
+ *
+ * <h3>Gerar Senha Definitiva</h3>
+ * Caso seja a 1ª vez que o usuário efetue login no sistema e possua senha temporária
+ * ativa, este deverá trocar a senha temporária por uma definitiva, que será informada
+ * pelo próprio usuário. <p>
+ *
+ * <h3>Criptografar Senha</h3>
+ * Após o sistema receber o usuário com a senha (definitiva) preenchida, o sistema
+ * deverá criptografar a senha para o correto armazenamento desta em algum banco de
+ * dados. <p>
+ *
+ * @author Jonatas Mateus
+ * @see UsuarioDTO
+ * @since 1.0
+ */
 @Service
 public class UsuarioService extends BaseService implements UserDetailsService {
 
