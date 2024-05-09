cc8963718c6c383fbb6efbfdfc0e258e64a1f17c
Jonatas <jonatasmateuscontato@gmail.com>
2024-05-09 16:18:01 -0300

    Classe GprojuridicoApplicationTests desabilitada para teste

--- src/test/java/com/uniprojecao/fabrica/gprojuridico/GprojuridicoApplicationTests.java
+++ src/test/java/com/uniprojecao/fabrica/gprojuridico/GprojuridicoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.uniprojecao.fabrica.gprojuridico;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class GprojuridicoApplicationTests {
 
 	@Test
