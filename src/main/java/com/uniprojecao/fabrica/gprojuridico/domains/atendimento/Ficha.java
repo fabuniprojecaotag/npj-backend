0e27c889aa849ba22f284e0989393b0541be7723
Jonatas <jonatasmateuscontato@gmail.com>
2024-08-02 21:52:07 -0300

    Ajuste temporário de vínculo Medida J. em Aten.

--- src/main/java/com/uniprojecao/fabrica/gprojuridico/domains/atendimento/Ficha.java
+++ src/main/java/com/uniprojecao/fabrica/gprojuridico/domains/atendimento/Ficha.java
@@ -27,16 +27,8 @@ public abstract class Ficha {
     public Ficha(String assinatura, String medidaJuridica, Boolean dadosSensiveis, List<Testemunha> testemunhas) {
         this.assinatura = assinatura;
         this.dadosSensiveis = dadosSensiveis;
-        setMedidaJuridica(medidaJuridica);
+        this.medidaJuridica = medidaJuridica;
         setTestemunhas(testemunhas);
-    }
-
-    public void setMedidaJuridica(String medidaJuridica) {
-        this.medidaJuridica = MedidaJuridica.valueOf(medidaJuridica
-                .replace(" – ", " ") // substitui travessão
-                .replace(" - ", " ") // substitui hífen
-                .replace(" ", "_")
-                .toUpperCase()).getNormalizedValue();
     }
 
     public void setTestemunhas(List<Testemunha> testemunhas) {
