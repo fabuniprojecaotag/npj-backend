bfe1141b8268cdab71dfe34e5d36088f5cbb2a33
Jonatas <jonatasmateuscontato@gmail.com>
2024-08-10 10:33:04 -0300

    Mudança de path do FilterType

--- src/main/java/com/uniprojecao/fabrica/gprojuridico/enums/FilterType.java
+++ src/main/java/com/uniprojecao/fabrica/gprojuridico/enums/FilterType.java
@@ -1,4 +1,4 @@
-package com.uniprojecao.fabrica.gprojuridico.domains.enums;
+package com.uniprojecao.fabrica.gprojuridico.enums;
 
 public enum FilterType {
     EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN, LESS_THAN_OR_EQUAL, NOT_EQUAL;
