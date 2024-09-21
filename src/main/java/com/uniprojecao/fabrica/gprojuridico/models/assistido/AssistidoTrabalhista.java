8304fc7a2643f048821c8937169a107dc5d49aca
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-09-21 11:06:19 -0300

    remoção de construtores não utilizados em assistidos

--- src/main/java/com/uniprojecao/fabrica/gprojuridico/models/assistido/AssistidoTrabalhista.java
+++ src/main/java/com/uniprojecao/fabrica/gprojuridico/models/assistido/AssistidoTrabalhista.java
@@ -1,6 +1,5 @@
 package com.uniprojecao.fabrica.gprojuridico.models.assistido;
 
-import com.uniprojecao.fabrica.gprojuridico.models.Endereco;
 import com.uniprojecao.fabrica.gprojuridico.models.atendimento.Ctps;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,8 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -24,39 +21,4 @@ public class AssistidoTrabalhista extends Assistido {
     @NotNull
     private Boolean empregadoAtualmente;
 
-    public AssistidoTrabalhista(
-            String nome,
-            String rg,
-            String cpf,
-            String nacionalidade,
-            String escolaridade,
-            String estadoCivil,
-            String profissao,
-            String telefone,
-            String email,
-            Filiacao filiacao,
-            String remuneracao,
-            Map<String, Endereco> endereco,
-            Ctps ctps,
-            String pis,
-            Boolean empregadoAtualmente
-    ) {
-        super(
-                nome,
-                rg,
-                cpf,
-                nacionalidade,
-                escolaridade,
-                estadoCivil,
-                profissao,
-                telefone,
-                email,
-                filiacao,
-                remuneracao,
-                endereco
-        );
-        this.ctps = ctps;
-        this.pis = pis;
-        this.empregadoAtualmente = empregadoAtualmente;
-    }
 }
