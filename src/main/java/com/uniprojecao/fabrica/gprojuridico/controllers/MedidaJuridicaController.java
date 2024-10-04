5b28a2a07c00cabcbde32b2e68fd8f1188e2dad1
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-10-04 17:32:41 -0300

    Adicionando as controllers restantes que faltaram

---
+++ src/main/java/com/uniprojecao/fabrica/gprojuridico/controllers/MedidaJuridicaController.java
@@ -0,0 +1,16 @@
+package com.uniprojecao.fabrica.gprojuridico.controllers;
+
+import com.uniprojecao.fabrica.gprojuridico.models.MedidaJuridica;
+import com.uniprojecao.fabrica.gprojuridico.services.MedidaJuridicaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/medidas juridicas")
+public class MedidaJuridicaController extends FirestoreController<MedidaJuridica> {
+    protected MedidaJuridicaController(MedidaJuridicaService genericService) {
+        super(genericService);
+    }
+}
+
+
